4f23c89af36b3216f44e2d7605f266c759411089
Marcelo Melo <marcellomello90@gmail.com>
2023-01-28 01:56:18 +0100

    structuring js folder

--- js/index.js
+++ js/index.js
@@ -1,48 +1,61 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
 
+const timer = Timer({
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset
+})
 
 buttonPlay.addEventListener('click', function() {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')
+  controls.play()
+  timer.countdown()
 })
 
 buttonPause.addEventListener('click', function() {
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
+  controls.pause()
+  timer.hold()
 })
 
 buttonStop.addEventListener('click', function() {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
+  controls.reset()
+  timer.reset()
 })
 
-buttonSoundOff.addEventListener('click', function(){
+buttonSoundOff.addEventListener('click', function() {
   buttonSoundOn.classList.remove('hide')
   buttonSoundOff.classList.add('hide')
-  
 })
 
-buttonSoundOn.addEventListener('click', function(){
+buttonSoundOn.addEventListener('click', function() {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
-  
 })
 
-buttonSet.addEventListener('click', function(){
-  minutes = prompt("Quantos minutos?")
-  minutesDisplay.textContent = minutes
-  
+buttonSet.addEventListener('click', function() {
+  let newMinutes = controls.getMinutes()
+
+  if (!newMinutes) {
+    timer.reset()
+    return
+  }
+
+  timer.updateDisplay(newMinutes, 0)
+  timer.updateMinutes(newMinutes)
 })
